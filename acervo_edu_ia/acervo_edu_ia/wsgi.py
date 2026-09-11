571eac6efeb3456f4465677a37be865d118a0d4d
Almerinda Maria <almerindamariafeitosabrros@gmail.com>
2026-09-11 15:33:40 -0300

    primeiro commit

---
+++ acervo_edu_ia/acervo_edu_ia/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for acervo_edu_ia project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'acervo_edu_ia.settings')
+
+application = get_wsgi_application()
